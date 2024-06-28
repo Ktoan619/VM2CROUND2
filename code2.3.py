038ae97418b81ee1fd7bd24edb60ac96fa7e19b5
Ktoan619 <133044715+Ktoan619@users.noreply.github.com>
2024-06-28 07:16:44 +0700

    Create code2.3.py

---
+++ code2.3.py
@@ -0,0 +1,52 @@
+
+import pandas as pd
+import gurobipy as gp
+from gurobipy import GRB
+
+m = 10
+n = 200
+
+Facility = pd.read_csv('Facility.txt', sep = '\t', header = None)
+Cost = pd.read_csv('TransCost.txt', sep = '\t', header = None)
+Demand = pd.read_csv('DemandAdd.txt', sep = '\t', header = None)
+
+Setup = [1, 3, 5, 7, 8]
+
+Cost = Cost.T
+
+Need = Demand[:][3]
+
+FacCost = Facility[:][4]
+
+FacPro = Facility[:][3]
+
+model = gp.Model('Facility problems')
+
+Choose = model.addVars(m, vtype = GRB.BINARY)
+
+Product = model.addVars(m, n, lb = 0, vtype = GRB.INTEGER)
+
+for x in Setup :
+    model.addConstr(Choose[x] == 1)
+
+for j in range(n) :
+    model.addConstr(gp.quicksum(Product[i, j] for i in range(m)) == Need[j])
+
+for i in range(m) :
+    model.addConstr(gp.quicksum(Product[i, j] for j in range(n)) <= FacPro[i]*Choose[i])
+
+
+model.setObjective(gp.quicksum(Choose[i]*FacCost[i] for i in range(m)) + gp.quicksum(Product[i, j]*Cost[i][j] for i in range(m) for j in range(n)), GRB.MINIMIZE)
+model.optimize()
+
+
+for i in range(m) :
+    if(Choose[i].x > 0) :
+        print(i+1, " ", FacCost[i], " ", gp.quicksum(Product[i, j].x for j in range(n)))
+
+for i  in range(m) : 
+    for j in range(n) :
+        if Product[i, j].x > 0 : print(i+1, ' ' , j, ' ', Product[i, j].x)
+
+print(model.ObjVal)
+
